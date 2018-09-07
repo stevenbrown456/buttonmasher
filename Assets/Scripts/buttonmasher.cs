1f69b24133b08ec3a5b56b4f9084ae3cba42aee1
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-09-07 16:46:43 +0100

    start method

---
+++ Assets/Scripts/buttonmasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonmasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start method call");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("end method call");
+		
+	}
+}
