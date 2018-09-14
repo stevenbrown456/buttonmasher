019f380e35b755b630205d8d59b8fdc3ec352a9a
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-09-14 14:24:04 +0100

    created buttons and score

--- Assets/Scripts/buttonmasher.cs
+++ Assets/Scripts/buttonmasher.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class buttonmasher : MonoBehaviour {
+    //data for class
+    public AudioSource clickSound; //sound that will play when click button
+    public TextMesh scoreText; //display text for score
 
-	// Use this for initialization
+
+    private int score = 0; //the numerical data value
+    
+    // Use this for initialization
 	void Start () {
         Debug.Log("start method call");
-	}
+	} // End of Start()
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("end method call");
-		
-	}
+        //End of update function()
+
+        
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+
+        clickSound.Play(); //Trigger our clicking sound 
+        //increase score by 1
+        score = score +1;
+        //update visual score 
+        scoreText.text = score.ToString();
+    }
 }
+//End of ButtonMasher
